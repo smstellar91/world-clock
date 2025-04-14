fd7dc05eab846538a0fd11a7756cfefbaa07fd20
smstellar91 <stellamarisap7@gmail.com>
2025-04-14 05:16:23 -0700

    added JS  functionality

--- javascript/index.js
+++ javascript/index.js
@@ -12,7 +12,7 @@ londoTimeElement.innerHTML=londonTime.format("h:mm:ss [<small>]A[</small>]");
 let parisElement = document.querySelector("#paris");
 let parisDateElement= parisElement.querySelector(".date");
 let parisTimeElement= parisElement.querySelector(".time");
-let parisTime=moment().tz("Europe/Parris");
+let parisTime=moment().tz("Europe/Paris");
 
 parisDateElement.innerHTML=parisTime.format("MMMM Do YYYY");
 parisTimeElement.innerHTML=parisTime.format("h:mm:ss [<small>]A[</small>]");
@@ -27,5 +27,23 @@ tokyoTimeElement.innerHTML=tokyoTime.format("h:mm:ss [<small>]A[</small>]");
 
 }
 
+function updateCity(event) {
+    let cityTimeZone= event.target.value;
+    let cityTime= moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML=`
+     <div class="city">
+        <div>
+        <h2>${cityTimeZone}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")} </div>
+    </div>
+        <div class="time"> ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>
+    `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement= document.querySelector("#city");
+citiesSelectElement.addEventListener("change",updateCity);
